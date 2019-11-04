43f487720626d5b9b3ae6685e39fea000fb96e32
Pedro Coelho [1131485] <1131485@isep.ipp.pt>
2019-11-04 21:20:40 +0000

    See #3 Created and documented the class which represents the Item in our buisness logic

---
+++ P1/src/GFAB/Model/Item/Item.cs
@@ -0,0 +1,60 @@
+using System;
+
+namespace GFAB.Model.Item {
+
+    //Entity that represents an item within the inventory
+    //Also the root of the aggregate Item --> represented in our aggregate/root diagram aswell
+    public class Item {
+
+        ///<summary>
+        /// Identifies a certain item in inventory. This is unique for each item
+        ///</summary>
+        public ItemID id {get; set;} 
+        ///<summary>
+        /// Represents the current location within the kitchen for a single item
+        ///</summary>
+        public Location location {get; set;}
+        ///<summary>
+        ///Represents the date which the item was produced
+        ///</summary>
+        public DateTime productionDate{get; set;}
+        ///<summary>
+        ///Represents the date for which the item can no longer be served
+        ///</summary>
+        public DateTime expirationDate {get; set;}
+        ///<summary>
+        /// Represents the period for which the item can be served in the cafeteria (for example)
+        ///</summary>
+        public TimePeriod livenessPeriod {get; set;}
+        ///<summary>
+        ///Flag which indicates if the item was already server or not
+        /// By default a item isnt served
+        ///</summary>
+        public bool served {get; set;} = false;
+        ///<summary>
+        ///The unique identification of the meal which is currently related to a single item 
+        ///</summary>
+        //public MealID mealId {get; set;}
+
+        ///<summary>
+        /// Method which will indicate if a item is avaliable at the moment or not
+        ///</summary>
+        public bool avaliable () {
+
+            if(!this.served) return true;
+            else return false;
+        }
+
+        ///<summary>
+        ///Method that will marked this item as served
+        ///Returns false if the item was already served
+        ///</summary>
+        public bool markAsServed() {
+            if(this.avaliable()) {
+                this.served = true;
+                return true;
+            }
+            else return false;
+        }
+    }
+}
