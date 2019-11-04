401250cc1de7932b56c332e9e9cd68d2a2a0223b
Pedro Coelho [1131485] <1131485@isep.ipp.pt>
2019-11-04 21:06:54 +0000

    See #3 Created Value Object Time Period in reference to Item entity

---
+++ P1/src/GFAB/Model/Item/TimePeriod.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace GFAB.Model.Item {
+
+    //Value Object
+    public class TimePeriod {
+        public DateTime startDate {get; set;}
+        public DateTime endDate {get; set;}
+    }
+}
