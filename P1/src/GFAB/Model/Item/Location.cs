ddc28336167caa041c056e8abf524b8141909624
Joao Freitas <1160907@isep.ipp.pt>
2019-11-05 14:01:07 +0000

    Updated Location to comply with Value Object pattern
    see #3

--- P1/src/GFAB/Model/Item/Location.cs
+++ P1/src/GFAB/Model/Item/Location.cs
@@ -1,6 +1,56 @@
-namespace GFAB.Model {
-    //Value Object
-    public class Location :  ValueObject{
-        public string name {get; set;}
+using System;
+
+namespace GFAB.Model
+{
+  /// <summary>
+  /// Location is a model that identifies a location (e.g. ISEP)
+  /// </summary>
+  public class Location : ValueObject
+  {
+
+    /// <summary>
+    /// Name indicates the location name
+    /// </summary>
+    /// <value></value>
+    // TODO: @Pedro Coelho remove set usage in order to comply with Value Object pattern
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Creates a Location based on a specified string
+    /// </summary>
+    /// <param name="name">A string that identifies the location name</param>
+    /// <returns>Location for the specified string</returns>
+    // TODO: Unit test this function
+	public static Location ValueOf(string name)
+    {
+      return new Location(name);
     }
+
+    /// <summary>
+    /// Private and only constructor allowed for Location
+    /// Object construction modifications should be done via ValueOf function variations
+    /// </summary>
+    /// <param name="name">The name that identifies the location</param>
+    private Location(string name)
+    {
+
+      grantNameCannotBeNull(name);
+
+      grantNameHasAtLeastThreeCharacters(name);
+
+      this.Name = name;
+    }
+
+    // TODO: @PedroCoelho implement this verification method (should throw ArgumentException if verification fails)
+    private void grantNameHasAtLeastThreeCharacters(string name)
+    {
+      throw new NotImplementedException();
+    }
+
+    // TODO: @PedroCoelho implement this verification method (should throw ArgumentException if verification fails)
+    private void grantNameCannotBeNull(string name)
+    {
+      throw new NotImplementedException();
+    }
+  }
 }
