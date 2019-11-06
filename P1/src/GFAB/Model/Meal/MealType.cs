7f1e0a297828f1d3771eb3c65f8b4d2f63fdadd9
Joao Freitas <1160907@isep.ipp.pt>
2019-11-06 01:02:37 +0000

    Added enum for meal type
    see #6

---
+++ P1/src/GFAB/Model/Meal/MealType.cs
@@ -0,0 +1,16 @@
+namespace GFAB.Model
+{
+
+  /// <summary>
+  /// An enumerator for the existing meal types
+  /// </summary>
+  public enum MealType
+  {
+
+    SOUP,
+    MAIN_COURSE,
+
+    DESSERT
+
+  }
+}
