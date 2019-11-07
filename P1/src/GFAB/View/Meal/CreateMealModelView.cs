c22e32ebf30176eb642faff9449f1c51238ace5e
Joao Freitas <1160907@isep.ipp.pt>
2019-11-07 23:32:57 +0000

    Added model view for create meal functionality
    see #10

---
+++ P1/src/GFAB/View/Meal/CreateMealModelView.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using GFAB.Model;
+using static GFAB.View.CreateMealModelView.CreateMealModelViewDescriptors;
+
+namespace GFAB.View{
+
+	[DataContract]
+	public class CreateMealModelView{
+
+		public string Designation;
+
+		public string Type;
+		
+		public List<string> Ingredients;
+
+		public List<string> Allergens;
+
+		public List<CreateMealModelViewDescriptors> Descriptors;
+
+
+		[CollectionDataContract]
+		public class CreateMealModelViewDescriptors : List<CreateMealModelViewDescriptorsElement>{
+
+			[DataContract]
+			public class CreateMealModelViewDescriptorsElement{
+
+				public string Name;
+
+				public double Quantity;
+
+				public string QuantityUnit;
+
+				public CreateMealModelViewDescriptorsElement(Descriptor descriptor){
+
+					Name = descriptor.Name;
+
+					Quantity = descriptor.Quantity;
+
+					QuantityUnit = descriptor.QuantityUnit;
+
+				}
+
+			}
+
+			public CreateMealModelViewDescriptors(List<Descriptor> descriptors){
+
+				foreach(Descriptor descriptor in descriptors){
+
+					CreateMealModelViewDescriptorsElement element = new CreateMealModelViewDescriptorsElement(descriptor);
+
+					Add(element);
+
+				}
+
+			}
+
+		}
+
+	}
+
+}
