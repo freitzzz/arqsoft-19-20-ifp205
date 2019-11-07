4a9e43ac727a070bb301fe5b265b50ea1e8787bd
Henrique Maciel <hnmmaciel@gmail.com>
2019-11-07 22:31:10 +0000

    Created initial solution for the Purchase Item interface. Refs #7

--- P1/src/gfaw/src/View/Components/CreateItem/CreateItem.jsx
+++ P1/src/gfaw/src/View/Components/CreateItem/CreateItem.jsx
@@ -3,33 +3,126 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles(theme => ({
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
+}));
 
 export default function AlertDialog(props) {
+    const classes = useStyles();
 
-  return (
-    <div>
-      <Dialog
-        open={props.open}
-        onClose={props.close}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{"Add a new Item to the Inventory"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={props.close} color="primary">
-            Cancel
+    //Selected Meal to serve as the base for the Item to be created
+    const [baseMeal, setBaseMeal] = React.useState('');
+
+    const handleMealChange = event => {
+        setBaseMeal(event.target.value);
+    };
+
+    //Selected Location state
+    const [selectedLocation, setSelectedLocation] = React.useState('');
+
+    const handleLocationChange = event => {
+        setSelectedLocation(event.target.value);
+    };
+
+    //Production Date state
+    const [selectedProdDate, setSelectedProdDate] = React.useState();
+
+    const handleProdDateChange = event => {
+        setSelectedProdDate(event.target.value);
+        console.log(event.target.value);
+    };
+
+    //Expiration Date state
+    const [selectedExpDate, setSelectedExpDate] = React.useState();
+
+    const handleExpDateChange = event => {
+        setSelectedExpDate(event.target.value);
+    };
+
+    //Label, Location, Exp Date, Prod Date, Identification number (?)
+
+    return (
+        <div>
+            <Dialog
+                open={props.open}
+                onClose={props.close}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <DialogTitle id="alert-dialog-title">{"Add a new Item to the Inventory"}</DialogTitle>
+                <DialogContent>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Meals</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={baseMeal}
+                            onChange={handleMealChange}
+                        >
+                            <MenuItem value={'meal1'}>Sopa de Pedra</MenuItem>
+                            <MenuItem value={'meal2'}>Frango</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel id="demo-simple-select-label">Location</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={selectedLocation}
+                            onChange={handleLocationChange}
+                        >
+                            <MenuItem value={'meal1'}>Edifício E</MenuItem>
+                            <MenuItem value={'meal2'}>Edifício H</MenuItem>
+                        </Select>
+                    </FormControl>
+                </DialogContent>
+                <DialogContent>
+                    <form className={classes.container} noValidate>
+                        <TextField
+                            id="prodDate"
+                            label="Production Date"
+                            type="date"
+                            className={classes.textField}
+                            onChange={handleProdDateChange}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                        <TextField
+                            id="expDate"
+                            label="Expiration Date"
+                            type="date"
+                            className={classes.textField}
+                            onChange={handleExpDateChange}
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                        />
+                    </form>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={props.close} color="primary">
+                        Cancel
           </Button>
-          <Button onClick={props.close} color="primary" autoFocus>
-            Confirm
+                    <Button onClick={props.close} color="primary" autoFocus>
+                        Confirm
           </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
-  );
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
 }
