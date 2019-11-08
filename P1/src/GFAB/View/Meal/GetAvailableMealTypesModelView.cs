85a58dddd72733f3a589f23feca50da97b888489
Joao Freitas <1160907@isep.ipp.pt>
2019-11-08 00:35:12 +0000

    Added model view for get available meal types functionality
    see #10

---
+++ P1/src/GFAB/View/Meal/GetAvailableMealTypesModelView.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using GFAB.Model;
+using static GFAB.View.GetAvailableMealTypesModelView;
+
+namespace GFAB.View{
+
+	[CollectionDataContract]
+	public class GetAvailableMealTypesModelView : List<GetAvailableMealTypesModelViewElement>{
+
+		[DataContract]
+		public class GetAvailableMealTypesModelViewElement{
+
+			public int Id;
+
+			public string Name;
+
+			public GetAvailableMealTypesModelViewElement(string mealType){
+
+				Name = mealType;
+
+			}
+		}
+
+		public GetAvailableMealTypesModelView(List<string> mealTypes){
+
+			for(int i=0; i<mealTypes.Count;i++){
+
+				string mealType = mealTypes[i];
+
+				GetAvailableMealTypesModelViewElement element = new GetAvailableMealTypesModelViewElement(mealType);
+
+				element.Id = i + 1;
+			}
+
+		}
+
+	}
+
+}
