812bf1943ab93535c1d65543de36225ae17a6b52
Joao Freitas <1160907@isep.ipp.pt>
2019-11-08 02:57:39 +0000

    Added SQLite3 implementation for RepositoryFactory
    see #12

---
+++ P1/src/GFAB/Controller/Repository/Impl/SQLite3/SQLite3RepositoryFactoryImpl.cs
@@ -0,0 +1,25 @@
+namespace GFAB.Controllers
+{
+
+  public class SQLite3RepositoryFactoryImpl : RepositoryFactory
+  {
+
+    private SQLite3DbContext ctx;
+
+    public SQLite3RepositoryFactoryImpl(SQLite3DbContext dbContext)
+    {
+      ctx = dbContext;
+    }
+
+    public ItemRepository ItemRepository()
+    {
+      return new SQLite3ItemRepositoryImpl(ctx);
+    }
+
+    public MealRepository MealRepository()
+    {
+      return new SQLite3MealRepositoryImpl(ctx);
+    }
+  }
+
+}
