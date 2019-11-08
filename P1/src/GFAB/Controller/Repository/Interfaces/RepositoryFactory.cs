103441b8362ffb82b8da24f9999cf08e508be04d
Joao Freitas <1160907@isep.ipp.pt>
2019-11-08 02:57:23 +0000

    Added factory for creating repositories
    see #12

---
+++ P1/src/GFAB/Controller/Repository/Interfaces/RepositoryFactory.cs
@@ -0,0 +1,15 @@
+namespace GFAB.Controllers
+{
+
+  /// <summary>
+  /// A factory that allows the creation of repositories
+  /// </summary>
+  public interface RepositoryFactory
+  {
+    MealRepository MealRepository();
+
+    ItemRepository ItemRepository();
+
+  }
+
+}
