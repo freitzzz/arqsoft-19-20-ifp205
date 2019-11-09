c84494796efce060dad492cff9c470f38e5c40c5
Joao Freitas <1160907@isep.ipp.pt>
2019-11-09 02:30:54 +0000

    Fixed create and delete item and added available items functionality
    see #13

--- P1/src/GFAB/Controller/HTTPController/ItemsController.cs
+++ P1/src/GFAB/Controller/HTTPController/ItemsController.cs
@@ -1,102 +1,117 @@
 using System;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Http;
-using GFAB.View.Item;
 using GFAB.View;
 using GFAB.Model;
+using System.Collections.Generic;
+
 namespace GFAB.Controllers
 {
 
-    [Route("api/items")]
-    [ApiController]
+  [Route("api/items")]
+  [ApiController]
 
-    public class ItemsController : ControllerBase //TODO: Create a technical logger to write in a .txt file locally on the server
+  public class ItemsController : ControllerBase //TODO: Create a technical logger to write in a .txt file locally on the server
+  {
+    private RepositoryFactory factory;
+
+    public ItemsController(RepositoryFactory repositoryFactory)
     {
-        private RepositoryFactory factory;
-
-        public ItemsController(RepositoryFactory repositoryFactory)
-        {
-            factory = repositoryFactory;
-        }
-
-        //POST: /items
-        [HttpPost]
-        public IActionResult RegisterItem(AddItemModelView itemToAdd)
-        {
-
-            if (!ModelState.IsValid || itemToAdd == null)
-            {
-                return BadRequest(new ErrorModelView("information not accepted"));
-            }
-
-            try
-            {
-                Meal meal = this.factory.MealRepository().Find(itemToAdd.mealId);
-
-                try
-                {
-                    Location location = Location.ValueOf(itemToAdd.location);
-                    DateTime expirationDate = DateTime.Parse(itemToAdd.expirationDate);
-                    DateTime productionDate = DateTime.Parse(itemToAdd.productionDate);
-                    try
-                    {
-                        Item item = new Item(meal.Id(), location, productionDate, expirationDate);
-
-                        try
-                        {
-                            this.factory.ItemRepository().Save(item);
-
-                            RegisterItemModelView response = new RegisterItemModelView(Convert.ToInt32(item.ID),
-                                Convert.ToInt32(meal.ID), item.id.Id,
-                                item.location.Name,
-                                item.livenessPeriod.EndDateTime.ToShortDateString(),
-                                item.productionDate.ToShortDateString(), item.expirationDate.ToShortDateString());
-
-                            return StatusCode(201, response);
-                        }
-                        catch (Exception databaseException)
-                        { 
-                            return StatusCode(500);
-                        }
-                    }
-                    catch (ArgumentException periodException)
-                    {
-
-                        return BadRequest(new ErrorModelView(periodException.Message));
-                    }
-                }
-                catch (ArgumentException exception)
-                {
-                    return BadRequest(new ErrorModelView(exception.Message));
-                }
-
-            }
-            catch (ArgumentException e)
-            {
-                return BadRequest(new ErrorModelView(e.Message));
-            }
-        }
-        
-        //Delete : /items:id
-        [HttpDelete("{id}")]
-        public IActionResult RemoveItem( long itemId)
-        {
-            try {
-                Item item = this.factory.ItemRepository().Find(itemId);
-
-                this.factory.ItemRepository().Delete(item);
-
-                return NoContent();
-            }
-            catch(Exception e) {
-
-                if(e is ArgumentException)
-                    return NotFound(new ErrorModelView(e.Message));
-
-                return StatusCode(500);
-            }
-            
-        }
+      factory = repositoryFactory;
     }
+
+
+    [HttpGet]
+    public IActionResult AvailableItems()
+    {
+      try
+      {
+        List<Item> items = factory.ItemRepository().All();
+
+        if (items.Count == 0)
+        {
+          return NotFound();
+        }
+
+        GetAvailableItemsModelView modelview = new GetAvailableItemsModelView(items);
+
+        return Ok(modelview);
+      }
+      catch (Exception databaseException)
+      {
+        // TODO: Log exception
+        return StatusCode(500);
+      }
+
+    }
+
+    //POST: /items
+    [HttpPost]
+    public IActionResult RegisterItem(AddItemModelView itemToAdd)
+    {
+
+      if (!ModelState.IsValid || itemToAdd == null)
+      {
+        return BadRequest(new ErrorModelView("information not accepted"));
+      }
+
+      try
+      {
+
+        Meal meal = this.factory.MealRepository().Find(itemToAdd.mealId);
+
+        string location = itemToAdd.location;
+        DateTime expirationDate = DateTime.Parse(itemToAdd.expirationDate);
+        DateTime productionDate = DateTime.Parse(itemToAdd.productionDate);
+
+        Item item = new Item(meal.Id(), location, productionDate, expirationDate);
+
+        this.factory.ItemRepository().Save(item);
+
+        RegisterItemModelView response = new RegisterItemModelView(item);
+
+        return StatusCode(201, response);
+      }
+      catch (InvalidOperationException argumentException)
+      {
+        // TODO: Log exception
+        return BadRequest(new ErrorModelView(argumentException.Message));
+
+      }
+      catch (ArgumentException argumentException)
+      {
+        // TODO: Log exception
+        return BadRequest(new ErrorModelView(argumentException.Message));
+
+      }
+      catch (Exception databaseException)
+      {
+        // TODO: Log exception
+        return StatusCode(500);
+
+      }
+    }
+
+    //Delete : /items:id
+    [HttpDelete("{id}")]
+    public IActionResult RemoveItem(long id)
+    {
+      try
+      {
+        Item item = this.factory.ItemRepository().Find(id);
+
+        this.factory.ItemRepository().Delete(item);
+
+        return NoContent();
+      }
+      catch (Exception e)
+      {
+
+        if (e is ArgumentException)
+          return NotFound();
+
+        return StatusCode(500);
+      }
+
+    }
+  }
 }
